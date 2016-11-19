8b92bfce9c698c703f7bae3ca3e252fe41af5276
Devin Zimmerman <dzimmerman902@gmail.com>
2016-11-19 10:33:26 -0500

    Saving spot

--- assets/javascript/onConnectAPI.js
+++ assets/javascript/onConnectAPI.js
@@ -3,11 +3,10 @@ $(document).ready(function () {
 	function zipCode () {
 
 		var query = {
-			zip: "32765",
-			api_key: "gpsby6z4y9kttvza5n2yyqha"
+			api_key: "26ghhgmvg4vrn4eq7ctg236f"
 		}
 
-		var queryURL = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-11-19&zip=78701&api_key=gpsby6z4y9kttvza5n2yyqha"
+		var queryURL = "http://data.tmsapi.com/v1.1/theatres?zip=78701&api_key=26ghhgmvg4vrn4eq7ctg236f"
 
 		console.log(queryURL);
 
@@ -18,9 +17,11 @@ $(document).ready(function () {
 
 		$.ajax(request).done(function(respone) {
 
+			console.log(response);
 
 		})
 	}
 
 	zipCode();
 })
+
